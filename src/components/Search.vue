28390bb73fa7d14b1b2e46c5c756659e3e65451c
Kerwin-Jin <1351207863@qq.com>
2021-07-19 23:36:57 +0800

    search by

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="jumbotron">
     <h1>Searh Github Users</h1>
-    <input type="text" class="form-control" placeholder="请输入用户名">
-    <button class="btn btn-primary btn-block">Search</button>
+    <input type="text" class="form-control" placeholder="请输入用户名" v-model="keyWord">
+    <button class="btn btn-primary" @click="search">Search</button>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
     name:"Search",
     data(){
         return{
-
+          keyWord:''
         }
+    },
+    methods:{
+      async search(){
+        try {
+          const resp = await axios.get("https://api.github.com/search/users",{params:{q:this.keyWord}});
+          const {items} = resp.data;
+          console.log(items);
+        } catch (error) {
+          console.log(error.message);
+        }
+      }
     }
 }
 </script>
